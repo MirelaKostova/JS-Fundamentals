08940ba45e948a9eadbbd7da8ab952b081264c1d
Mirela Kostova <mir.s.kostova@gmail.comm>
2022-06-16 11:06:43 +0300

    equalArrays

---
+++ Arrays - Lab/equalArrays.js
@@ -0,0 +1,20 @@
+function equalArrays(arr1, arr2) {
+  let areEqual = true;
+  let index = null;
+  let sum = 0;
+
+  for (let i = 0, k = 0; i < arr1.length; i++, k++) {
+    if (arr1[i] !== arr2[k]) {
+      areEqual = false;
+      index = i;
+      break;
+    }
+    sum += Number(arr1[i]);
+  }
+
+  if (areEqual) {
+    console.log(`Arrays are identical. Sum: ${sum}`);
+  } else {
+    console.log(`Arrays are not identical. Found difference at ${index} index`);
+  }
+}
