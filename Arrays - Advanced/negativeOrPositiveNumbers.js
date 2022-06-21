90631139562ed42d1c1ed8a88b49f5417bae1139
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-06-21 12:40:59 +0300

    negativeOrPositiveNumbers

--- Arrays - Advanced/negativeOrPositiveNumbers.js
+++ Arrays - Advanced/negativeOrPositiveNumbers.js
@@ -1,1 +1,15 @@
-function negativeOrPositiveNumbers() {}
+function negativeOrPositiveNumbers(array) {
+  let result = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < 0) {
+      result.unshift(array[i]);
+    } else {
+      result.push(array[i]);
+    }
+  }
+
+  console.log(result.join("\n"));
+}
+
+negativeOrPositiveNumbers(["7", "-2", "8", "9"]);
