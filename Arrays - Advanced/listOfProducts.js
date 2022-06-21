1d807df48cbc9b14917b45fe896f537937de34ae
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-06-21 16:25:51 +0300

    listOfProducts

--- Arrays - Advanced/listOfProducts.js
+++ Arrays - Advanced/listOfProducts.js
@@ -1,1 +1,9 @@
-function listOfProducts() {}
+function listOfProducts(products) {
+  let sortedByName = products.sort();
+
+  for (let i = 0; i < sortedByName.length; i++) {
+    console.log(`${i + 1}.${sortedByName[i]}`);
+  }
+}
+
+listOfProducts(["Potatoes", "Tomatoes", "Onions", "Apples"]);
