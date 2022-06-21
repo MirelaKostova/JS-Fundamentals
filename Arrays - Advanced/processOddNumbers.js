4abde1f0d64783b042da4f887be2b9ba124df9af
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-06-21 15:28:44 +0300

    updated

--- Arrays - Advanced/processOddNumbers.js
+++ Arrays - Advanced/processOddNumbers.js
@@ -1,3 +1,10 @@
-function processOddNumbers(arr) {}
+function processOddNumbers(arr) {
+  let filteredElements = arr.filter((el, i) => i % 2 != 0);
+  let oddNums = filteredElements.map((x) => x * 2);
+  let result = oddNums.reverse();
+
+  console.log(result.join(" "));
+}
 
 processOddNumbers([10, 15, 20, 25]);
+processOddNumbers([3, 0, 10, 4, 7, 3]);
