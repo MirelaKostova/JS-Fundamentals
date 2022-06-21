4bfc4a6e117c5bd10646b1354ebb5ea8eb830253
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-06-21 12:36:02 +0300

    sumFirstAndLast

---
+++ Arrays - Advanced/sumFirstAndLast.js
@@ -0,0 +1,9 @@
+function sumFirstAndLast(array) {
+  let firstNum = Number(array.shift());
+  let lastNum = Number(array.pop());
+  let sum = firstNum + lastNum;
+
+  console.log(sum);
+}
+
+sumFirstAndLast(["20", "30", "40"]);
