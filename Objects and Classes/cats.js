fdb0e589e8f7d42c1283a838c7e923ee1b533d4e
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-07-05 15:50:55 +0300

    cats

---
+++ Objects and Classes/cats.js
@@ -0,0 +1,26 @@
+function cats(catsData) {
+  class Cat {
+    constructor(catName, age) {
+      this.catName = catName;
+      this.age = age;
+    }
+
+    meow() {
+      console.log(`${this.catName}, age ${this.age} says Meow`);
+    }
+  }
+
+  let catArr = [];
+
+  for (let el of catsData) {
+    let currCat = el.split(" ");
+    let currCatName = currCat[0];
+    let currCatAge = currCat[1];
+
+    let newCat = new Cat(currCatName, currCatAge);
+    catArr.push(newCat);
+  }
+
+  console.log(catArr);
+}
+cats(["Mellow 2", "Tom 5"]);
