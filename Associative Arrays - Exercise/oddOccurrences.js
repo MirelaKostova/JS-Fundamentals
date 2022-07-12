7304c0662d6e5f5593d42ee357ab9a754ecb7675
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-07-12 15:56:41 +0300

    updated

--- Associative Arrays - Exercise/oddOccurrences.js
+++ Associative Arrays - Exercise/oddOccurrences.js
@@ -1,5 +1,25 @@
 function oddOccurrences(sentence) {
-  let;
+  let elements = sentence.split(" ").map((el) => el.toLowerCase());
+  let newSentence = new Map();
+  //   console.log(elements);
+
+  for (const currEl of elements) {
+    // console.log("currEl -->", currEl);
+
+    let wordsCounter = elements.filter((el) => el == currEl).length;
+    newSentence.set(currEl, wordsCounter);
+    // console.log("wordsCounter -->", wordsCounter);
+  }
+  //   console.log(newSentence.entries());
+
+  let result = Array.from(newSentence.entries());
+  let oddRes = result.filter((el) => el[1] % 2 !== 0);
+
+  let printLine = "";
+  for (let [key, value] of oddRes) {
+    printLine += key + " ";
+  }
+  console.log(printLine);
 }
 
 oddOccurrences("Java C# Php PHP Java PhP 3 C# 3 1 5 C#");
