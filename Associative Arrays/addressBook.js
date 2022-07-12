dd494470195c3d6c8d88328fa4d68bd1f7d9a6d9
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-07-12 09:43:37 +0300

    addressBook

--- Associative Arrays/addressBook.js
+++ Associative Arrays/addressBook.js
@@ -1,15 +1,18 @@
 function addressBook(dataArray) {
   let addressRegister = {};
 
+  // Loop through the array, split it and create variables for each line.
   for (const line of dataArray) {
     let [name, address] = line.split(":");
     addressRegister[name] = address;
   }
 
+  //  Convert object to array by Object.entries and sorted alphabeticall by the person’s name
   let sortedRegister = Object.entries(addressRegister).sort((a, b) =>
     a[0].localeCompare(b[0])
   );
 
+  //   Loop through the array and print the full list.
   for (const person of sortedRegister) {
     console.log(`${person[0]} -> ${person[1]}`);
   }
