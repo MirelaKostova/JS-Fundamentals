231e5a3e95d2f32d84f0934e3adffd1f63f3f12d
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-07-14 17:37:02 +0300

    aMinerTask

---
+++ Associative Arrays - Exercise/aMinerTask.js
@@ -0,0 +1,23 @@
+function aMinerTask(input) {
+  let collection = {};
+  //   console.log("input ->", input);
+  for (let i = 0; i < input.length; i++) {
+    let resource = input[i];
+    let quantity = Number(input[++i]);
+
+    // console.log("resource ->", resource);
+    // console.log("quantity->", quantity);
+
+    if (!collection.hasOwnProperty(resource)) {
+      collection[resource] = quantity;
+    } else {
+      collection[resource] += quantity;
+    }
+  }
+
+  for (const resource in collection) {
+    console.log(resource, "->", collection[resource]);
+  }
+}
+
+aMinerTask(["Gold", "155", "Silver", "10", "Copper", "17"]);
