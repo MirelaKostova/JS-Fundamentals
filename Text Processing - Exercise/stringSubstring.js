c610b078db7f40d10ee348b4db73f77bfef4eb68
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-07-18 11:45:09 +0300

    stringSubstring

--- Text Processing - Exercise/stringSubstring.js
+++ Text Processing - Exercise/stringSubstring.js
@@ -1,1 +1,16 @@
-function stringSubstring() {}
+function stringSubstring(word, text) {
+  const wordToLowerCase = word.toLowerCase();
+  const textToLowerCase = text.toLowerCase().split(" ");
+
+  if (!textToLowerCase.includes(wordToLowerCase)) {
+    console.log(`${word} not found!`);
+  } else {
+    for (const el of textToLowerCase) {
+      if (el === wordToLowerCase) {
+        console.log(word);
+      }
+    }
+  }
+}
+
+stringSubstring("javascript", "JavaScript is the best programming language");
