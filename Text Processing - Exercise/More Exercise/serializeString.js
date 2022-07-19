f1730710b6df92c954e334868346f01120b542b4
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-07-19 10:31:37 +0300

    serializeString

--- Text Processing - Exercise/More Exercise/serializeString.js
+++ Text Processing - Exercise/More Exercise/serializeString.js
@@ -1,1 +1,20 @@
-function serializeString() {}
+function serializeString(input) {
+  let charObj = {};
+  let string = input[0];
+
+  for (let i = 0; i < string.length; i++) {
+    let currChar = string[i];
+
+    if (!charObj.hasOwnProperty(currChar)) {
+      charObj[currChar] = [];
+    }
+
+    charObj[currChar].push(i);
+  }
+
+  for (const ch in charObj) {
+    console.log(`${ch}:${charObj[ch].join("/")}`);
+  }
+}
+
+serializeString(["abababa"]);
