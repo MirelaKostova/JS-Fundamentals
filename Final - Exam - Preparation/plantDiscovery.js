907db25472e272783089ba5a0a1ba76d2e089bf3
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-07-21 14:27:03 +0300

    plantDiscovery

--- Final - Exam - Preparation/plantDiscovery.js
+++ Final - Exam - Preparation/plantDiscovery.js
@@ -1,17 +1,22 @@
 function plantDiscovery(input) {
   let actions = {
-    Rare: () => {},
-    Update: () => {},
-    Reset: () => {},
+    Rate: rate,
+    Update: update,
+    Reset: reset,
   };
 
-  //  parse input and create plant catalog
+  //  create plant catalog.
   let catalog = {};
 
+  //  get the number for next n lines
   let n = Number(input.shift());
 
+  // parse input
   for (let i = 0; i < n; i++) {
     let [name, rarity] = input.shift().split("<->");
+    // store the information in catalog
+    // - parse rarity to Number (we need to sorted by rarity)
+    // - store all ratings => [];
     catalog[name] = {
       rarity: Number(rarity),
       rate: [],
@@ -20,35 +25,57 @@ function plantDiscovery(input) {
   }
 
   // update catolog by given commands
-
   while (input[0] !== "Exhibition") {
     let [command, params] = input.shift().split(": ");
     let action = actions[command];
     action(params);
   }
-  // sort catalog
 
   //print result
+  console.log("Plants for the exhibition:");
+  for (let plant of Object.entries(catalog)) {
+    // console.log(plant);
+    console.log(
+      `- ${plant[0]}; Rarity: ${
+        plant[1].rarity
+      }; Rating: ${plant[1].avgRate.toFixed(2)}`
+    );
+  }
 
   function rate(line) {
     let [name, rating] = line.split(" - ");
     if (catalog[name] !== undefined) {
       let plant = catalog[name];
-      plant.ratings.push();
+      plant.rate.push(Number(rating));
 
       let total = 0;
-      for (let rating of plant.ratings) {
+      for (let rating of plant.rate) {
         total += rating;
       }
 
-      plant.avgRate = total / plant.ratings.length;
+      plant.avgRate = total / plant.rate.length;
     } else {
       console.log("error");
     }
   }
 
-  function update(line) {}
-  function reset(line) {}
+  function update(line) {
+    let [name, rarity] = line.split(" - ");
+    if (catalog[name] !== undefined) {
+      catalog[name].rarity = Number(rarity);
+    } else {
+      console.log("error");
+    }
+  }
+
+  function reset(name) {
+    if (catalog[name] !== undefined) {
+      catalog[name].rate.length = 0;
+      catalog[name].avgRate = 0;
+    } else {
+      console.log("error");
+    }
+  }
 }
 
 plantDiscovery([
