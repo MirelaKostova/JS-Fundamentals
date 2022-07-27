a42d748bcf4e1f9f9eeb73317011c7faeb890d1e
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-07-27 12:12:22 +0300

    adAstra

--- Final - Exam - Preparation/adAstra.js
+++ Final - Exam - Preparation/adAstra.js
@@ -20,7 +20,6 @@ function adAstra(input) {
     valid = pattern.exec(input);
   }
 
-  //   console.log(totalCalories);
   let days = totalCalories / 2000;
 
   console.log(`You have food to last you for: ${Math.floor(days)} days!`);
@@ -37,6 +36,5 @@ function adAstra(input) {
 // ]);
 
 adAstra([
-  //   "$$#@@%^&#Fish#24/12/20#8500#|#Incorrect#19.03.20#450|$5*(@!#IceCream#03/10/21#9000#^#@aswe|Milk|05/09/20|2000|",
   "$$#@@%^&#Fish#24/12/20#8500#|#Incorrect#19.03.20#450|$5*(@!#Ice Cream#03/10/21#9000#^#@aswe|Milk|05/09/20|2000|",
 ]);
