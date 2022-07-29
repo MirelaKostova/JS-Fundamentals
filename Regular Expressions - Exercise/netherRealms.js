73dd9bf36551f3511e04a1d963d073fc34c34a8d
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-07-29 14:07:29 +0300

    netherRealms

--- Regular Expressions - Exercise/netherRealms.js
+++ Regular Expressions - Exercise/netherRealms.js
@@ -6,10 +6,7 @@ function netherRealms(input) {
   let demons = {};
 
   for (let el of txt) {
-    demons[el] = {
-      demonsHealh: demonsHealh,
-      damage: damage.toFixed(2),
-    };
+    let demonName = el;
 
     let onlyLetters = el.replace(/[^A-Za-z]/g, "");
 
@@ -18,7 +15,6 @@ function netherRealms(input) {
     }
 
     let pattern = /[+-]?([0-9]*[.])?[0-9]+/g;
-
     while ((match = pattern.exec(el)) !== null) {
       damage += Number(match[0]);
     }
@@ -34,12 +30,21 @@ function netherRealms(input) {
         damage = damage * 2;
       }
     }
+
+    demons[demonName] = {
+      demonsHealh: demonsHealh,
+      damage: damage.toFixed(2),
+    };
+
+    demonsHealh = 0;
+    damage = 0;
   }
 
   //sorted alphabetically
   let sorted = Object.entries(demons).sort((a, b) => a[0].localeCompare(b[0]));
-
+  // console.log(sorted);
   for (let demon of sorted) {
+    // console.log(demon);
     console.log(
       `${demon[0]} - ${demons[demon[0]].demonsHealh} health, ${
         demons[demon[0]].damage
@@ -48,8 +53,9 @@ function netherRealms(input) {
   }
 }
 
-netherRealms("M3ph-0.5s-0.5t0.0**");
-console.log("----------");
+// netherRealms("M3ph-0.5s-0.5t0.0**");
+// console.log("----------");
 netherRealms("M3ph1st0**, Azazel");
-console.log("----------");
-netherRealms("Gos/ho");
+// console.log("----------");
+// netherRealms("Gos/ho");
+// netherRealms("Azazel");
