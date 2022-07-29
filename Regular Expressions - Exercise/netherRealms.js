73400b545ee5ebb9c669be16554385cc0f3225df
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-07-29 10:59:54 +0300

    netherRealms

---
+++ Regular Expressions - Exercise/netherRealms.js
@@ -0,0 +1,13 @@
+function netherRealms(txt) {
+  let demonsHealh = 0;
+  let onlyLetters = txt.replace(/[^A-Za-z]/g, "");
+
+  for (let currLetter of onlyLetters) {
+    demonsHealh += Number(currLetter.charCodeAt(0));
+  }
+
+  let damage = 0;
+  let allNumbers = txt.console.log(sum);
+}
+
+netherRealms("M3ph-0.5s-0.5t0.0**");
