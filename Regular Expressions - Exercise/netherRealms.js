31ab2a0476f277823b54d054517bb8628dcf2c53
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-08-01 10:43:09 +0300

    netherRealms

--- Regular Expressions - Exercise/netherRealms.js
+++ Regular Expressions - Exercise/netherRealms.js
@@ -1,61 +1,53 @@
 function netherRealms(input) {
-  let txt = input.split(/[ ,]+/g);
-  let demonsHealh = 0;
-  let damage = 0;
+  let demonsArray = input.split(/[ ,]+/g);
+  // console.log(demonsArray);
 
   let demons = {};
 
-  for (let el of txt) {
-    let demonName = el;
+  for (let demon of demonsArray) {
+    let demonsHealh = 0;
+    let damage = 0;
 
-    let onlyLetters = el.replace(/[^A-Za-z]/g, "");
+    let onlyLetters = demon.replace(/[^A-Za-z]/g, "");
 
     for (let currLetter of onlyLetters) {
       demonsHealh += Number(currLetter.charCodeAt(0));
     }
 
+    let isMatch = false;
     let pattern = /[+-]?([0-9]*[.])?[0-9]+/g;
-    while ((match = pattern.exec(el)) !== null) {
+    while ((match = pattern.exec(demon)) !== null) {
+      isMatch = true;
       damage += Number(match[0]);
     }
 
-    if (el.includes("/")) {
-      while (el.includes("/")) {
-        el = el.replace("/", "");
-        damage = damage / 2;
-      }
-    } else if (el.includes("*")) {
-      while (el.includes("*")) {
-        el = el.replace("*", "");
-        damage = damage * 2;
+    if (isMatch) {
+      if (demon.includes("/") || demon.includes("*")) {
+        demon === "*" ? (damage *= 2) : (damage /= 2);
       }
     }
 
-    demons[demonName] = {
+    demons[demon] = {
       demonsHealh: demonsHealh,
       damage: damage.toFixed(2),
     };
-
-    demonsHealh = 0;
-    damage = 0;
   }
 
-  //sorted alphabetically
-  let sorted = Object.entries(demons).sort((a, b) => a[0].localeCompare(b[0]));
-  // console.log(sorted);
-  for (let demon of sorted) {
-    // console.log(demon);
-    console.log(
-      `${demon[0]} - ${demons[demon[0]].demonsHealh} health, ${
-        demons[demon[0]].damage
-      } damage`
-    );
-  }
+  //print sorted alphabetically
+  Object.entries(demons)
+    .sort((a, b) => a[0].localeCompare(b[0]))
+    .forEach(([demon, data]) => {
+      console.log(
+        `${demon} - ${data.demonsHealh} health, ${data.damage} damage`
+      );
+    });
 }
 
-// netherRealms("M3ph-0.5s-0.5t0.0**");
-// console.log("----------");
+netherRealms("M3ph-0.5s-0.5t0.0**");
+console.log("----------");
 netherRealms("M3ph1st0**, Azazel");
-// console.log("----------");
-// netherRealms("Gos/ho");
+console.log("----------");
+netherRealms("Gos/ho");
 // netherRealms("Azazel");
+
+// netherRealms("C3ph1st0**, Azazel     Hsgfaufaf Bkljij, Dih");
