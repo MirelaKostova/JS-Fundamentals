c738f97ef2e527ac0defbe2ef263393d65307e35
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-08-03 15:03:22 +0300

    schoolRegister

--- Objects and Classes - Exercise/More Exercise/schoolRegister.js
+++ Objects and Classes - Exercise/More Exercise/schoolRegister.js
@@ -1,99 +1,46 @@
 function schoolRegister(array) {
   // Load students list from array
-  let studentsRegister = [];
+  let studentsRegister = {};
 
-  for (const el of array) {
-    let splitedArr = el.split(", ");
-
-    let name = splitedArr[0].split(" ").pop();
-    let grade = splitedArr[1].split(" ").pop();
-    let score = Number(splitedArr[2].split(" ").pop());
+  //  "Student name: Mark, Grade: 8, Graduated with an average score: 4.75",
+  for (let line of array) {
+    let tokens = line.split(", ");
+    let name = tokens[0].split(": ").pop();
+    let grade = Number(tokens[1].split(": ").pop()) + 1;
+    let score = Number(tokens[2].split(": ").pop());
 
     // 'If a student has a lower score than 3, he does not go into the next class'
     if (score >= 3) {
-      grade++;
+      let student = {
+        name: name,
+        grade: grade,
+        score: score,
+      };
 
-      let student = { name, grade, score };
-      studentsRegister.push(student);
-    }
-  }
-  //   // console.log("listOfStudents -->", listOfStudents);
-
-  // ---------------------------------------------
-  // School register sorted in ascending order by grade
-
-  studentsRegister.sort((a, b) => a.grade - b.grade);
-  console.log("listOfStudents -->", listOfStudents);
-
-  function averageAnnualScore(studentsRegister) {
-    return listOfStudents.reduce(
-      (a, b) => (a.score + b.score, 0) / listOfStudents.length
-    );
-  }
-}
-
-// for (const element of listOfStudents) {
-//   const students = element.name;
-//   console.log(`${element.grade} Grade`);
-//   //   console.log(
-//   //     `List of students: ${listOfStudents
-//   //       .map((el) => el.listOfStudents)
-//   //       .join(", ")}`
-//   //   );
-//   //   // console.log(
-//   //   //   `Average annual score from last year: ${average(
-//   //   //     students.map((s) => s.score)
-//   //   //   ).toFixed(2)}\n`
-//   //   // );
-
-///////////////////////////////////////
-
-// `{nextGrade} Grade
-
-// List of students: {All students in that grade}
-
-// Average annual score from last year: {average annual score on the entire class from last year}`
-// }
-
-function schoolRegister(input) {
-  const register = {};
-
-  for (let line of input) {
-    let tokens = line.split(", ");
-
-    let student = tokens[0].split(" ").pop();
-    let grade = Number(tokens[1].split(" ").pop()) + 1;
-    let score = Number(tokens[2].split(" ").pop());
-
-    if (score >= 3) {
-      let profile = { student, score };
-
-      if (!register.hasOwnProperty(grade)) {
-        register[grade] = [];
+      if (!studentsRegister.hasOwnProperty(grade)) {
+        studentsRegister[grade] = [];
       }
 
-      register[grade].push(profile);
+      studentsRegister[grade].push(student);
     }
   }
 
-  const grades = Object.keys(register);
-
-  for (const grade of grades) {
-    const students = register[grade];
-
+  for (let [grade, students] of Object.entries(studentsRegister)) {
+    // Grade
     console.log(`${grade} Grade`);
-    console.log(
-      `List of students: ${students.map((s) => s.student).join(", ")}`
-    );
-    console.log(
-      `Average annual score from last year: ${average(
-        students.map((s) => s.score)
-      ).toFixed(2)}\n`
-    );
-  }
 
-  function average(arr) {
-    return arr.reduce((a, b) => a + b, 0) / arr.length;
+    // All students in that grade
+    let listOfStudents = [];
+    students.map((students) => listOfStudents.push(students.name));
+
+    // Average annual score
+    let avgScore = 0;
+    students.map((a) => (avgScore += a.score));
+    avgScore /= listOfStudents.length;
+
+    console.log(`List of students: ${listOfStudents.join(", ")}\n`);
+
+    console.log(`Average annual score from last year: ${avgScore.toFixed(2)}`);
   }
 }
 
