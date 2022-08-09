e4761ea765159ee041f1b4463de9c7fcbe40e9db
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-08-09 13:40:46 +0300

    memoryGame

--- Mid - Exam - Preparation/memoryGame.js
+++ Mid - Exam - Preparation/memoryGame.js
@@ -1,8 +1,56 @@
 function memoryGame(input) {
-  let sequenceOfEl = input.shift().map(Number);
+  let sequenceOfEl = input.shift().split(" ");
+  let moves = 0;
+  let isWinner = false;
 
-  while (input[0] !== "end") {
-    let currIndexes = input.shift().split(" ").map(Number);
+  for (let line of input) {
+    if (line === "end") {
+      break;
+    }
+
+    let [index1, index2] = line.split(" ").map(Number);
+    moves++;
+    let isEqual = index1 === index2;
+    let isNotInBounds =
+      index1 < 0 ||
+      index1 >= sequenceOfEl.length ||
+      index2 < 0 ||
+      index2 >= sequenceOfEl.length;
+
+    if (isEqual || isNotInBounds) {
+      console.log("Invalid input! Adding additional elements to the board");
+      let elToAdd = `-${moves}a`;
+      let index = Math.floor(sequenceOfEl.length / 2);
+      sequenceOfEl.splice(index, 0, elToAdd, elToAdd);
+    }
+
+    if (sequenceOfEl[index1] == sequenceOfEl[index2]) {
+      console.log(
+        `Congrats! You have found matching elements - ${sequenceOfEl[index1]}!`
+      );
+      sequenceOfEl = sequenceOfEl.filter((el) => el !== sequenceOfEl[index1]);
+    } else if (sequenceOfEl[index1] !== sequenceOfEl[index2]) {
+      console.log("Try again!");
+    }
+
+    if (sequenceOfEl.length <= 0) {
+      isWinner = true;
+      break;
+    }
+
+    // console.log(sequenceOfEl);
+  }
+
+  if (sequenceOfEl.length <= 0) {
+    isWinner = true;
+  }
+
+  if (isWinner) {
+    console.log(`You have won in ${moves} turns!`);
+  } else {
+    console.log("Sorry you lose :(\n" + `${sequenceOfEl.join(" ")}`);
   }
 }
-memoryGame(["1 1 2 2 3 3 4 4 5 5", "1 0", "-1 0", "1 0", "1 0", "1 0", "end"]);
+// memoryGame(["1 1 2 2 3 3 4 4 5 5", "1 0", "-1 0", "1 0", "1 0", "1 0", "end"]);
+memoryGame(["a 2 4 a 2 4", "0 3", "0 2", "0 1", "0 1", "end"]);
+// memoryGame(["a 2 4 a 2 4", "4 0", "0 2", "0 1", "0 1", "end"]);
