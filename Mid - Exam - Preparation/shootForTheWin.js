250abd10f54d3fc28bb774c7f45e24fffbcf2a03
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-08-10 09:25:11 +0300

    shootForTheWin

--- Mid - Exam - Preparation/shootForTheWin.js
+++ Mid - Exam - Preparation/shootForTheWin.js
@@ -5,25 +5,28 @@ function shootForTheWin(input) {
   while (input[0] !== "End") {
     let index = Number(input.shift());
     let currValue = sequence[index];
-    let isValid = currValue !== undefined && currValue != "-1";
+    let isValid = currValue !== -1 && index >= 0 && index < sequence.length;
 
     if (isValid) {
       countOfShotTargets++;
       sequence[index] = -1;
 
-      sequence.forEach((el) => {
-        if (el !== sequence[index]) {
-          if (el > currValue) {
-            sequence[sequence.indexOf(el)] -= currValue;
+      for (let i = 0; i < sequence.length; i++) {
+        let currTarget = sequence[i];
+
+        if (currTarget !== -1) {
+          if (currTarget > currValue) {
+            currTarget -= currValue;
           } else {
-            sequence[sequence.indexOf(el)] += currValue;
+            currTarget += currValue;
           }
         }
-      });
+        sequence[i] = currTarget;
+      }
     }
   }
 
-  console.log(`Shot targets: ${countOfShotTargets} -> ${sequence.join(" ")}`);
+  return `Shot targets: ${countOfShotTargets} -> ${sequence.join(" ")}`;
 }
 
 shootForTheWin(["24 50 36 70", "0", "4", "3", "1", "End"]);
