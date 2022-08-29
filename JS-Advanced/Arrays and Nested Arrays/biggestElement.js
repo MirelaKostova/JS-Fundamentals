e2bd42a107c52dae97c2e0fd2d6f9bbe7698e84f
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-08-29 15:20:25 +0300

    biggestElement

---
+++ JS-Advanced/Arrays and Nested Arrays/biggestElement.js
@@ -0,0 +1,16 @@
+function biggestElement(input) {
+  return Math.max(...input.flat());
+}
+
+biggestElement([
+  [20, 50, 10],
+  [8, 33, 145],
+]);
+//145
+
+biggestElement([
+  [3, 5, 7, 12],
+  [-1, 4, 33, 2],
+  [8, 3, 0, 4],
+]);
+//33
