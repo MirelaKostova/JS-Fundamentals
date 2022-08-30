d6fe8fbac50ce49d57768a80b1ef98a117384575
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-08-30 09:51:11 +0300

    diagonalSums

---
+++ JS-Advanced/Arrays and Nested Arrays/diagonalSums.js
@@ -0,0 +1,25 @@
+function diagonalSums(input) {
+  let indexL = 0;
+  let indexR = input.length - 1;
+
+  let firstDiagonal = 0;
+  let secondDiagonal = 0;
+
+  for (const el of input) {
+    firstDiagonal += el[indexL++];
+    secondDiagonal += el[indexR--];
+  }
+
+  console.log(firstDiagonal + " " + secondDiagonal);
+}
+
+diagonalSums([
+  [20, 40],
+  [10, 60],
+]);
+
+diagonalSums([
+  [3, 5, 17],
+  [-1, 7, 14],
+  [1, -8, 89],
+]);
