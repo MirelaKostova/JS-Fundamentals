dc44e4f789aefe17de2d03873d4c7bbcf508213c
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-08-31 15:19:11 +0300

    magicMatrices

--- JS-Advanced/Arrays and Nested Arrays - Exercise/magicMatrices.js
+++ JS-Advanced/Arrays and Nested Arrays - Exercise/magicMatrices.js
@@ -1,9 +1,30 @@
 function magicMatrices(matrix) {
-  let isMagical = false;
+  let isMagical = true;
+
+  for (let i = 0; i < matrix.length - 1; i++) {
+    let currRowSum = matrix[i].reduce((a, b) => a + b, 0);
+    let nextRowSum = matrix[i + 1].reduce((a, b) => a + b, 0);
+
+    let currColSum = 0;
+    let nextColSum = 0;
+
+    for (let j = 0; j < matrix.length; j++) {
+      currColSum += matrix[i][j];
+      nextColSum += matrix[i + 1][j];
+    }
+
+    if (currRowSum !== nextRowSum || currColSum !== nextColSum) {
+      isMagical = false;
+    }
+  }
+
+  return isMagical;
 }
 
-magicMatrices([
-  [4, 5, 6],
-  [6, 5, 4],
-  [5, 5, 5],
-]);
+console.log(
+  magicMatrices([
+    [4, 5, 6],
+    [6, 5, 4],
+    [5, 5, 5],
+  ])
+);
