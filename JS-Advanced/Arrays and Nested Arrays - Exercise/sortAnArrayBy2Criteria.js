b09933264f0878b355310d06d6ca69679ef6ff09
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-08-31 13:58:32 +0300

    sortAnArrayBy2Criteria

---
+++ JS-Advanced/Arrays and Nested Arrays - Exercise/sortAnArrayBy2Criteria.js
@@ -0,0 +1,35 @@
+function sortAnArrayBy2Criteria(input) {
+  input
+    .map((el) => el.toLowerCase())
+    .sort((prev, curr) => compareFn(prev, curr));
+
+  // primary criteria: by a length in ascending order
+  // second criteria: by alphabetical value in ascending order
+
+  console.log(input);
+
+  function compareFn(prev, curr) {
+    if (prev.length > curr.length) {
+      return -1;
+    } else if (prev.length < curr.length) {
+      return 1;
+    } 
+
+    if()
+    {
+      curr.localeCompare(prev);
+    }
+  }
+}
+
+sortAnArrayBy2Criteria(["alpha", "beta", "gamma"]);
+// beta
+// alpha
+// gamma
+
+sortAnArrayBy2Criteria(["Isacc", "Theodor", "Jack", "Harrison", "George"]);
+// Jack
+// Isacc
+// George
+// Theodor
+// Harrison
