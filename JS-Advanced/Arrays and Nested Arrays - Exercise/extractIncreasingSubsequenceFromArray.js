45b93c959016cd5c466c5d2592d0546e25751791
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-08-31 12:19:15 +0300

    extractIncreasingSubsequenceFromArray

---
+++ JS-Advanced/Arrays and Nested Arrays - Exercise/extractIncreasingSubsequenceFromArray.js
@@ -0,0 +1,24 @@
+function extractIncreasingSubsequenceFromArray(input) {
+  // reduce((previousValue, currentValue) => { /* … */ } )
+
+  const result = input.reduce((previousValue, currentValue) => {
+    const maxNumber = previousValue[previousValue.length - 1];
+
+    if (currentValue >= maxNumber || previousValue.length == 0) {
+      previousValue.push(currentValue);
+    }
+
+    return previousValue;
+  }, []);
+
+  console.log(result);
+}
+
+extractIncreasingSubsequenceFromArray([1, 3, 8, 4, 10, 12, 3, 2, 24]);
+// [1, 3, 8, 10, 12, 24]
+
+extractIncreasingSubsequenceFromArray([1, 2, 3, 4]);
+// [1, 2, 3, 4]
+
+extractIncreasingSubsequenceFromArray([20, 3, 2, 15, 6, 1]);
+// [20]
