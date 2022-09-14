be100bcaa79e6962aa252e807e938f164d253b57
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-09-14 16:50:09 +0300

    aggregateElements

--- JS-Advanced/Syntax, Functions and Statements/aggregateElements.js
+++ JS-Advanced/Syntax, Functions and Statements/aggregateElements.js
@@ -1,3 +1,31 @@
+function aggregateElements(numbers) {
+  aggregate(numbers, 0, (a, b) => a + b);
+  aggregate(numbers, 0, (a, b) => a + 1 / b);
+  aggregate(numbers, "", (a, b) => a + b);
+
+  function aggregate(numbers, initVal, func) {
+    let value = initVal;
+
+    for (let i = 0; i < numbers.length; i++) {
+      value = func(value, numbers[i]);
+    }
+    console.log(value);
+  }
+}
+
+aggregateElements([1, 2, 3]);
+// 6
+// 1.8333333333333333
+// 123
+
+console.log("-----------------");
+
+aggregateElements([2, 4, 8, 16]);
+// 30
+// 0.9375
+// 24816
+
+/*
 function aggregateElements(array) {
   let sumOfElements = 0;
   let sumOfInveresedValues = 0;
@@ -13,15 +41,4 @@ function aggregateElements(array) {
   console.log(sumOfInveresedValues);
   console.log(concatElements);
 }
-
-aggregateElements([1, 2, 3]);
-// 6
-// 1.8333333333333333
-// 123
-
-console.log("-----------------");
-
-aggregateElements([2, 4, 8, 16]);
-// 30
-// 0.9375
-// 24816
+*/
