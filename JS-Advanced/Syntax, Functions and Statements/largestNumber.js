4651ac085d9186d33ab82ca0ad91bef9c404cd43
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-09-14 15:10:05 +0300

    largestNumber

--- JS-Advanced/Syntax, Functions and Statements/largestNumber.js
+++ JS-Advanced/Syntax, Functions and Statements/largestNumber.js
@@ -1,8 +1,14 @@
+function largestNumber(...params) {
+  console.log(`The largest number is ${Math.max(...params)}.`);
+}
+
+largestNumber(5, -3, 16);
+largestNumber(-3, -5, -22.5);
+
+/*
 function largestNumber(num1, num2, num3) {
   let largestNumber = Math.max(num1, num2, num3);
 
   console.log(`The largest number is ${largestNumber}.`);
 }
-
-largestNumber(5, -3, 16);
-largestNumber(-3, -5, -22.5);
+*/
