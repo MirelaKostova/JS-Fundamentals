659ae87d3db09fbc45041d0c767bca01c3bb8258
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-09-15 14:35:40 +0300

    Optimized rotateArray solution

--- JS-Advanced/Arrays and Nested Arrays - Exercise/rotateArray.js
+++ JS-Advanced/Arrays and Nested Arrays - Exercise/rotateArray.js
@@ -1,5 +1,23 @@
 function rotateArray(array, num) {
   let newArray = [...array];
+  let rotations = num % newArray.length;
+
+  for (let i = 0; i < rotations; i++) {
+    newArray.unshift(newArray.pop());
+  }
+
+  console.log(newArray.join(" "));
+}
+
+rotateArray(["1", "2", "3", "4"], 2);
+
+console.log("----------");
+rotateArray(["Banana", "Orange", "Coconut", "Apple"], 15);
+
+// --------- second solution ---------
+/*
+function rotateArray(array, num) {
+  let newArray = [...array];
 
   for (let i = 0; i < num; i++) {
     let elToMove = newArray.pop();
@@ -8,7 +26,4 @@ function rotateArray(array, num) {
 
   console.log(newArray.join(" "));
 }
-
-rotateArray(["1", "2", "3", "4"], 2);
-
-rotateArray(["Banana", "Orange", "Coconut", "Apple"], 15);
+*/
