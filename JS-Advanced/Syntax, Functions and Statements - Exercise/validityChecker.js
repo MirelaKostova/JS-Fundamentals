d68f29f3d1e760e7cc1e55185dfa79cf1fa75a29
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-09-15 14:14:25 +0300

    validityChecker

--- JS-Advanced/Syntax, Functions and Statements - Exercise/validityChecker.js
+++ JS-Advanced/Syntax, Functions and Statements - Exercise/validityChecker.js
@@ -1,4 +1,20 @@
-function validityChecker(x1, y1, x2, y2) {}
+function validityChecker(x1, y1, x2, y2) {
+  let first = Math.sqrt(x1 ** 2 + y1 ** 2);
+  let second = Math.sqrt(x2 ** 2 + y2 ** 2);
+  let third = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+
+  console.log(`{${x1}, ${y1}} to {0, 0} is ${isValid(first)}`);
+  console.log(`{${x2}, ${y2}} to {0, 0} is ${isValid(second)}`);
+  console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is ${isValid(third)}`);
+
+  function isValid(distance) {
+    if (Number.isInteger(distance)) {
+      return "valid";
+    } else {
+      return "invalid";
+    }
+  }
+}
 
 validityChecker(3, 0, 0, 4);
 // {3, 0} to {0, 0} is valid
