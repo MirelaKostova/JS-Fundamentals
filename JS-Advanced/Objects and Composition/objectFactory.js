037671c1e41e16a486324fbc85966f00e59f1bd2
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-09-20 15:29:00 +0300

    Update

---
+++ JS-Advanced/Objects and Composition/objectFactory.js
@@ -0,0 +1,23 @@
+function objectFactory() {}
+
+const orders = [
+  {
+    template: { name: "ACME Printer" },
+    parts: ["print"],
+  },
+  {
+    template: { name: "Initech Scanner" },
+    parts: ["scan"],
+  },
+  {
+    template: { name: "ComTron Copier" },
+    parts: ["scan", "print"],
+  },
+  {
+    template: { name: "BoomBox Stereo" },
+    parts: ["play"],
+  },
+];
+
+const products = factory(library, orders);
+console.log(products);
