8a5eb5ebab9b9ebf8f917dcaf343cd074aa821ed
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-09-28 16:57:12 +0300

    Sum Numbers fixed

--- JS-Advanced/DOM Introduction/03.Sum Numbers/sumNumbers.js
+++ JS-Advanced/DOM Introduction/03.Sum Numbers/sumNumbers.js
@@ -1,1 +1,8 @@
-function sumNumbers()
+function calc() {
+  let num1 = document.getElementById("num1").value;
+  let num2 = document.getElementById("num2").value;
+  //   console.log("num1 ->", num1);
+  //   console.log("num2 ->", num2);
+  let sum = Number(num1) + Number(num2);
+  document.getElementById("sum").value = sum;
+}
