d13a5692e4bd45384dd7fd5318b99e639590719e
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-09-28 16:24:37 +0300

    Added extraxtText function

--- JS-Advanced/DOM Introduction/02.Collect List Items/collectListItems.js
+++ JS-Advanced/DOM Introduction/02.Collect List Items/collectListItems.js
@@ -1,1 +1,12 @@
-function extractText() {}
+function extractText(text) {
+  const items = document.querySelectorAll("ul#items li");
+  const textArea = document.querySelector("#result");
+
+  //   console.log("textArea:", textArea);
+
+  for (let currItem of items) {
+    //   textArea.value += currItem.textContent;
+    // console.log(currItem.textContent);
+    textArea.value += currItem.textContent + "\n";
+  }
+}
