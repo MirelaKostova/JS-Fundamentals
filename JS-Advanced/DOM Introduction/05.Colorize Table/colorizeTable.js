279be10265c64b9f375f36f912cedde57aa46b2b
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-09-29 12:21:16 +0300

    Colorize Table task

---
+++ JS-Advanced/DOM Introduction/05.Colorize Table/colorizeTable.js
@@ -0,0 +1,8 @@
+function colorize() {
+  const rows = document.querySelectorAll("table tr");
+  for (let i = 1; i < rows.length; i++) {
+    if (i % 2 !== 0) {
+      rows[i].style.background = "teal";
+    }
+  }
+}
