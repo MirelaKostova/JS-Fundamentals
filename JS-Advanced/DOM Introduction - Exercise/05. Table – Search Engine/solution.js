32eb942450f99715fa3e4a2b45c9c04a3cdf3540
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-10-02 10:25:41 +0300

    Updated solve function

--- JS-Advanced/DOM Introduction - Exercise/05. Table – Search Engine/solution.js
+++ JS-Advanced/DOM Introduction - Exercise/05. Table – Search Engine/solution.js
@@ -1,5 +1,17 @@
 function solve() {
   document.querySelector("#searchBtn").addEventListener("click", onClick);
 
-  function onClick() {}
+  function onClick() {
+    const stringToCheck = document.getElementById("searchField").value;
+    const dataRows = document.querySelectorAll("tbody tr");
+
+    document.getElementById("searchField").value = "";
+
+    for (const currRow of dataRows) {
+      currRow.classList.remove("select");
+      if (currRow.innerText.includes(stringToCheck)) {
+        currRow.className = "select";
+      }
+    }
+  }
 }
