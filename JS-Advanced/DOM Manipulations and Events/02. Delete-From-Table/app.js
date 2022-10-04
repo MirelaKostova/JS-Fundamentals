baef3338697cdaed6878c2c8e903135f4a5880a4
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-10-04 17:16:26 +0300

    Added function deleteByEmail()

--- JS-Advanced/DOM Manipulations and Events/02. Delete-From-Table/app.js
+++ JS-Advanced/DOM Manipulations and Events/02. Delete-From-Table/app.js
@@ -1,3 +1,16 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+  const mails = document.querySelectorAll("tr td:nth-of-type(2)");
+  const emailInput = document.querySelector("input[name='email']");
+  const result = document.querySelector("#result");
+
+  const matchedEmail = Array.from(mails).find(
+    (x) => x.textContent === emailInput.value
+  );
+
+  if (matchedEmail) {
+    matchedEmail.parentNode.remove();
+    result.textContent = "Deleted";
+  } else {
+    result.textContent = "Not found.";
+  }
 }
