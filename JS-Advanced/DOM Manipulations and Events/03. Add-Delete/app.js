1c0c5d13edaa790e5db9f80ef6b66d264a34bb9a
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-10-05 13:21:30 +0300

    Added  addItem function.

--- JS-Advanced/DOM Manipulations and Events/03. Add-Delete/app.js
+++ JS-Advanced/DOM Manipulations and Events/03. Add-Delete/app.js
@@ -1,3 +1,19 @@
 function addItem() {
-    //TODO...
+  let inputTextElement = document.getElementById("newItemText");
+  let itemsElements = document.getElementById("items");
+
+  let newLiElement = document.createElement("li");
+  newLiElement.textContent = inputTextElement.value;
+  inputTextElement.value = "";
+
+  let deleteElem = newLiElement.appendChild(document.createElement("a"));
+  deleteElem.href = "#";
+  deleteElem.textContent = "[Delete]";
+
+  deleteElem.addEventListener("click", (e) => {
+    e.currentTarget.parentElement.remove();
+  });
+
+  newLiElement.appendChild(deleteElem);
+  itemsElements.appendChild(newLiElement);
 }
