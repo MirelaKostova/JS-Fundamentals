bfbde8e03851d8368f0d95a9cc44d56d91cca312
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-10-05 14:12:17 +0300

    Added mouseoverHandler and mouseoutrHandler to the gradientElement of attachGradientEvents().

--- JS-Advanced/DOM Manipulations and Events/04. Mouse-Gradient/app.js
+++ JS-Advanced/DOM Manipulations and Events/04. Mouse-Gradient/app.js
@@ -1,3 +1,17 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+  let gradientElement = document.getElementById("gradient");
+  let result = document.getElementById("result");
+
+  const mouseoverHandler = (e) => {
+    console.log(e);
+    let persantage = Math.trunc((e.offsetX / e.target.clientWidth) * 100);
+    result.textContent = `${persantage}%`;
+  };
+
+  const mouseoutrHandler = (e) => {
+    result.textContent = "";
+  };
+
+  gradientElement.addEventListener("mousemove", mouseoverHandler);
+  gradientElement.addEventListener("mouseout", mouseoutrHandler);
 }
