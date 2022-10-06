e0fcef3dcbe99ca73b216f99486d80e4d2bd90cd
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-10-06 14:55:00 +0300

    Added onFocus function

--- JS-Advanced/DOM Manipulations and Events/05. Highlight-Active/app.js
+++ JS-Advanced/DOM Manipulations and Events/05. Highlight-Active/app.js
@@ -1,3 +1,11 @@
 function focused() {
-    console.log('TODO:...');
+  const sectionsElement = document.querySelectorAll("div input");
+
+  const onFocus = (e) => e.currentTarget.parentNode.classList.add("focused");
+  const onBlur = (e) => e.currentTarget.parentNode.classList.remove("focused");
+
+  for (const element of sectionsElement) {
+    element.addEventListener("focus", onFocus);
+    element.addEventListener("blur", onBlur);
+  }
 }
