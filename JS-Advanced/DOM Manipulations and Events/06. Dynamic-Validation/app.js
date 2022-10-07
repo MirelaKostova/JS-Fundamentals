53fc1b66e32928762e67a0cae746293d3496911c
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-10-07 10:09:56 +0300

    Added validate mail function

--- JS-Advanced/DOM Manipulations and Events/06. Dynamic-Validation/app.js
+++ JS-Advanced/DOM Manipulations and Events/06. Dynamic-Validation/app.js
@@ -1,3 +1,14 @@
 function validate() {
-    console.log('TODO:...');
+  const inputEmail = document.getElementById("email");
+  const pattern = /[a-z]+@[a-z]+.[a-z]+/;
+
+  inputEmail.addEventListener("change", isValid);
+
+  function isValid(e) {
+    if (!inputEmail.value.match(pattern)) {
+      inputEmail.classList.add("error");
+    } else {
+      inputEmail.classList.remove("error");
+    }
+  }
 }
