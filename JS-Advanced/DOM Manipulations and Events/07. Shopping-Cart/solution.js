3a194a6e29080be88065a5954d0ef43d4761419d
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-10-07 10:59:38 +0300

    Updated

--- JS-Advanced/DOM Manipulations and Events/07. Shopping-Cart/solution.js
+++ JS-Advanced/DOM Manipulations and Events/07. Shopping-Cart/solution.js
@@ -1,3 +1,40 @@
 function solve() {
-   //TODO...
+  const allAddButtons = document.querySelectorAll(".add-product");
+  const checkOutButton = document.querySelector(".checkout");
+  const resultArea = document.querySelector("textarea");
+  const listOfProducts = [];
+  const prices = [];
+
+  for (const currButton of allAddButtons) {
+    currButton.addEventListener("click", addProduct);
+  }
+
+  checkOutButton.addEventListener("click", checkout);
+
+  function addProduct(e) {
+    let currProduct = e.currentTarget.parentNode.parentNode;
+    let productName = currProduct.querySelector(".product-title");
+    let price = currProduct.querySelector(".product-line-price");
+
+    if (!listOfProducts.includes(productName.textContent)) {
+      listOfProducts.push(productName.textContent);
+    }
+
+    prices.push(Number(price.textContent));
+
+    resultArea.textContent += `Added ${productName.textContent} for ${price.textContent} to the cart.\n`;
+  }
+
+  function checkout(e) {
+    let totalPrice = prices.reduce((a, b) => a + b, 0);
+
+    resultArea.textContent += `You bought ${listOfProducts.join(
+      ", "
+    )} for ${totalPrice.toFixed(2)}.`;
+
+    for (const currButton of allAddButtons) {
+      currButton.setAttribute("disabled", "");
+      checkOutButton.setAttribute("disabled", "");
+    }
+  }
 }
