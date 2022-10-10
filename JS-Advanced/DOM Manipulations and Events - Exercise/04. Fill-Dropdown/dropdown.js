da0f08d300a45c6cbb8c79241278cf83beaea429
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-10-10 13:20:02 +0300

    Filled dropdown menu

--- JS-Advanced/DOM Manipulations and Events - Exercise/04. Fill-Dropdown/dropdown.js
+++ JS-Advanced/DOM Manipulations and Events - Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,13 @@
 function addItem() {
-    console.log('TODO:...');
+  const text = document.getElementById("newItemText").value;
+  const newItemValue = document.getElementById("newItemValue").value;
+  const dropDownMenu = document.getElementById("menu");
+
+  const newOptionElement = document.createElement("option");
+  newOptionElement.textContent = text;
+  newOptionElement.value = newItemValue;
+  dropDownMenu.appendChild(newOptionElement);
+
+  document.getElementById("newItemText").value = "";
+  document.getElementById("newItemValue").value = "";
 }
