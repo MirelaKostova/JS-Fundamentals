d873960d224c7cb2fa3518bc01b7ae2e39864c88
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-10-10 17:04:37 +0300

    Added encodeAndDecodeMessages()

--- JS-Advanced/DOM Manipulations and Events - Exercise/05. Encode-and-Decode-Messages/app.js
+++ JS-Advanced/DOM Manipulations and Events - Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,28 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+  const encodeArea = document.querySelector("#main > div:nth-child(1)");
+  const decodeArea = document.querySelector("#main > div:nth-child(2)");
+  const textAreaEncode = encodeArea.querySelector("textarea");
+  const textAreaDecode = decodeArea.querySelector("textarea");
+
+  encodeArea.querySelector("button").addEventListener("click", encode);
+  decodeArea.querySelector("button").addEventListener("click", decode);
+
+  let decodedMessage = "";
+
+  function encode(e) {
+    // console.log("textAreaEncode.value ->", textAreaEncode);
+    // console.log("textAreaDecode ->", textAreaDecode.value);
+    decodedMessage = textAreaEncode.value;
+    let encodedMsg = "";
+    for (let i = 0; i < decodedMessage.length; i++) {
+      const currChar = decodedMessage[i].charCodeAt();
+      encodedMsg += String.fromCharCode(currChar + 1);
+    }
+    textAreaDecode.value = encodedMsg;
+    textAreaEncode.value = "";
+  }
+
+  function decode() {
+    textAreaDecode.value = decodedMessage;
+  }
 }
