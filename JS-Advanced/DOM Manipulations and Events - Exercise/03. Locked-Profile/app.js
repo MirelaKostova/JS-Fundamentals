93b16caa3810e3cc0e9c4270f782dda6cd2317d9
Mirela Kostova <106656146+MirelaKostova@users.noreply.github.com>
2022-10-10 11:56:35 +0300

    Added lockedProfile

--- JS-Advanced/DOM Manipulations and Events - Exercise/03. Locked-Profile/app.js
+++ JS-Advanced/DOM Manipulations and Events - Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,21 @@
 function lockedProfile() {
-    console.log('TODO...')
+  const buttons = Array.from(document.querySelectorAll("button"));
+  buttons.forEach((button) => button.addEventListener("click", showMore));
+
+  function showMore(e) {
+    const currButton = e.target;
+    const currProfile = currButton.parentNode;
+    const isUnlocked = currProfile.querySelector('input[value="unlock"]');
+    const hiddenFields = currProfile.querySelector("div");
+
+    if (isUnlocked.checked) {
+      if (currButton.textContent === "Show more") {
+        hiddenFields.style.display = "inline-block";
+        e.target.textContent = "Hide it";
+      } else {
+        hiddenFields.style.display = "none";
+        e.target.textContent = "Show more";
+      }
+    }
+  }
 }
